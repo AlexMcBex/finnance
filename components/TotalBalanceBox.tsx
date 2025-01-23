33158f4f2cf753bc0adab2b7fe2d1e3e39188016
alexmcbex <alessiobelli94@gmail.com>
2025-01-23 15:22:34 -0500

    implement doughnut chart

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,12 +1,13 @@
 import { formatAmount } from '@/lib/utils'
 import React from 'react'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({accounts = [], totalBanks, totalCurrentBalance}: TotlaBalanceBoxProps) => {
   return (
     <section className="total-balance">
         <div className="total-balance-chart">
-            
+            <DoughnutChart  accounts={accounts}/>
         </div>
         <div className="flex flex-col gap-6">
             <h2 className="header-2">
@@ -14,9 +15,9 @@ const TotalBalanceBox = ({accounts = [], totalBanks, totalCurrentBalance}: Totla
             </h2>
             <div className="flex flex-col gap-2">
                 <p className="total-balance-label">Current total balance</p>
-                <p className="total-balance-amount flex-center gap-2">
+                <div className="total-balance-amount flex-center gap-2">
                     <AnimatedCounter  amount={totalCurrentBalance}/>
-                </p>
+                </div>
             </div>
         </div>
     </section>
