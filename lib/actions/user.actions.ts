1baa0c6150fc893bd4f35c33d061c47ed8b3e648
alexmcbex <alessiobelli94@gmail.com>
2025-02-24 16:24:55 -0500

    implemented appwrite to SignUp user.action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,18 +1,49 @@
-'use server'
+"use server";
 
-export const signIn = async () =>{
-    try{
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
 
-    }catch(err){
-        console.log("ERROR: " , err )
-    }
-}
+export const signIn = async () => {
+  try {
+  } catch (err) {
+    console.log("ERROR: ", err);
+  }
+};
 
+export const signUp = async (userData: SignUpParams) => { //https://appwrite.io/docs/tutorials/nextjs-ssr-auth/step-5
+    const {email, password, firstName, lastName} = userData //destructuring
+  try {
+    const { account } = await createAdminClient();
 
-export const signUp = async (userData: SignUpParams) =>{
-    try{
-// Appwrite to  create account
-    }catch(err){
-        console.log("ERROR: " , err )
-    }
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, { // "appwrite-session" has to be the same as in lib/appwrite.ts
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify(newUserAccount);
+  } catch (err) {
+    console.log("ERROR: ", err);
+  }
+};
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    return await account.get();
+  } catch (error) {
+    return null;
+  }
 }
