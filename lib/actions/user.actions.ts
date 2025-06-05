97e6786c1b45c47f476aa3729efe5cb483b2c6c4
alexmcbex <alessiobelli94@gmail.com>
2025-06-05 11:23:13 -0400

    fixed sign in action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -20,12 +20,38 @@ const {
   APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
 } = process.env;
 
+export const getUserInfo = async ({ userId }: getUserInfoProps) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const user = await database.listDocuments(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      [Query.equal("userId", [userId])]
+    );
+    return parseStringify(user.documents[0]);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const signIn = async ({ email, password }: signInProps) => {
   try {
     const { account } = await createAdminClient();
-    const response = await account.createEmailPasswordSession(email, password);
 
-    return parseStringify(response);
+    const session = await account.createEmailPasswordSession(email, password);
+
+    cookies().set("appwrite-session", session.secret, {
+      // "appwrite-session" has to be the same as in lib/appwrite.ts
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+    // const response = await account.createEmailPasswordSession(email, password);
+    const useer = await getUserInfo({ userId: session.userId });
+
+    return parseStringify(user);
   } catch (err) {
     console.log("ERROR: ", err);
   }
