6fb6cff793a0dcf352ef28b51d90f7670fb36019
alexmcbex <alessiobelli94@gmail.com>
2025-06-24 14:35:03 -0400

    added category chip for styling

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -8,7 +8,27 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { formatAmount, formatDateTime, getTransactionStatus, removeSpecialCharacters } from "@/lib/utils";
+import {
+  cn,
+  formatAmount,
+  formatDateTime,
+  getTransactionStatus,
+  removeSpecialCharacters,
+} from "@/lib/utils";
+import { transactionCategoryStyles } from "@/constants";
+
+const CategoryBadge = ({ category }: CategoryBadgeProps) => {
+  const { borderColor, backgroundColor, textColor, chipBackgroundColor } =
+    transactionCategoryStyles[
+      category as keyof typeof transactionCategoryStyles
+    ] || transactionCategoryStyles.default;
+  return (
+    <div className={cn("category-badge", borderColor, chipBackgroundColor, )}>
+      <div className={cn("size-2 rounded-full", backgroundColor )} />
+      <p className={cn("text-[12px] font-medium", textColor)}>{category}</p>
+    </div>
+  );
+};
 
 const TransactionsTable = ({ transactions }: TransactionTableProps) => {
   return (
@@ -29,21 +49,34 @@ const TransactionsTable = ({ transactions }: TransactionTableProps) => {
           const amount = formatAmount(t.amount);
           const isDebit = t.type === "debit";
           const isCredit = t.type === "credit";
-        //   console.log(t)
+          //   console.log(t)
           return (
-            <TableRow key={t.id} className={`${isDebit || amount[0] === "-" ? `bg-[#FFFBFA]` : `bg-[#F6FEF9]`} !over:bg-none !boder-b-default`}>
+            <TableRow
+              key={t.id}
+              className={`${
+                isDebit || amount[0] === "-" ? `bg-[#FFFBFA]` : `bg-[#F6FEF9]`
+              } !over:bg-none !boder-b-default`}
+            >
               <TableCell className="max-w-[25opx] pl-2 pr-10">
                 <div className="flex items-center gap-3">
-                  <h1 className="text-14 truncate font-semibold text-[#344054]">{removeSpecialCharacters(t.name)}</h1>
+                  <h1 className="text-14 truncate font-semibold text-[#344054]">
+                    {removeSpecialCharacters(t.name)}
+                  </h1>
                 </div>
               </TableCell>
 
-              <TableCell className={ `pl-2 pr-10 font-semibold ${isDebit || amount[0] === "-" ? `text-[#f04438]` : `text-[#039855]`} `}>
-                {isDebit ? `-${amount}` :  isCredit ? amount : amount }
+              <TableCell
+                className={`pl-2 pr-10 font-semibold ${
+                  isDebit || amount[0] === "-"
+                    ? `text-[#f04438]`
+                    : `text-[#039855]`
+                } `}
+              >
+                {isDebit ? `-${amount}` : isCredit ? amount : amount}
               </TableCell>
 
               <TableCell className="pl-2 pr-10">
-                {status}
+                <CategoryBadge category={status} />
               </TableCell>
 
               <TableCell className="min-w-32 pl-2 pr-10">
@@ -55,9 +88,8 @@ const TransactionsTable = ({ transactions }: TransactionTableProps) => {
               </TableCell>
 
               <TableCell className="pl-2 pr-10 max-md:hidden">
-                {t.category}
+                <CategoryBadge category={t.category} />
               </TableCell>
-
             </TableRow>
           );
         })}
